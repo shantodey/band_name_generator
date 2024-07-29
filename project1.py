daa925648ea42cb2f77cb387bf77fe597688ecda
Shanto Dey <126257294+shantodey@users.noreply.github.com>
2024-07-29 20:24:47 +0600

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,6 @@
+# my fast project that Band name generatror using python 
+
+print("welcome to the Band name generatror.")
+neme_of_city = input("What's the name of the city you grew up in?")
+name_of_pet = input("What's your pets name?")
+print("your band name could be " + neme_of_city + " " + name_of_pet) 
